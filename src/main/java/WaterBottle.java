565834804dba88cd4e1e6a0e599d652b9c32629c
CodeScott00 <dougal_00@hotmail.com>
2021-03-15 17:24:06 +0000

    waterbottle class created, tests written and then passed

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,25 @@
 public class WaterBottle {
+
+    private int volume;
+
+    public WaterBottle(int volume){
+        this.volume = volume;
+    }
+
+    //drink function takes 10
+    public int drink(){
+        this.volume = this.volume - 10;
+        return this.volume;
+    }
+    //empty function empties to 0
+    public int empty(){
+        return this.volume = 0;
+    }
+    //fill function to 100
+    public int full(){
+        return this.volume = 100;
+    }
 }
+
+
+
